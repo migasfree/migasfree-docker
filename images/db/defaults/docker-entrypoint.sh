a8ca97ee3e1df8bb48180e6ed6d4f9f0865cc3f7
alberto <alberto@migasfree.org>
2016-04-10 23:33:18 +0200

    updated image db

--- images/db/defaults/docker-entrypoint.sh
+++ images/db/defaults/docker-entrypoint.sh
@@ -1,12 +1,142 @@
 #!/bin/bash
 set -e
 
-echo "00 00 * * * /usr/bin/backup" > /tmp/cron
-crontab /tmp/cron 
-rm /tmp/cron
-service cron restart
+function set_TZ {
+    if [ -z "$TZ" ]; then
+      TZ="Europe/Madrid"
+    fi
+    # /etc/timezone for TZ setting
+    ln -fs /usr/share/zoneinfo/$TZ /etc/localtime || :
+}
 
-    
+function cron_init
+{
+    if [ -z "$POSTGRES_CRON" ]; then
+        POSTGRES_CRON="0 0 * * *"
+    fi
+    CRON=$(echo "$POSTGRES_CRON" |tr -d "'") # remove single quote
+    echo "$CRON /usr/bin/backup-db" > /tmp/cron
+    crontab /tmp/cron
+    rm /tmp/cron
+    service cron start
+}
+
+
+function get_pg_major_version()
+{
+    echo -n $(psql --version | head -1 | cut -d ' ' -f 3 | cut -d '.' -f1,2)
+}
+
+
+function set_listen_addresses() {
+    sedEscapedValue="$(echo "$1" | sed 's/[\/&]/\\&/g')"
+    sed -ri "s/^#?(listen_addresses\s*=\s*)\S+/\1'$sedEscapedValue'/" $(get_pg_config)
+}
+
+
+function is_pg_cluster_exists()
+{
+    pg_lsclusters | grep $PGDATA
+    test $? -eq 0
+}
+
+
+function pg__data_init
+{
+    # Create postgres data directory and run initdb if needed
+
+     is_pg_cluster_exists || (
+        echo "Initializing database files in $PGDATA"
+        chown postgres:postgres $PGDATA
+
+        pg_createcluster --locale en_US.UTF-8 --start $(get_pg_major_version) main -d $PGDATA
+
+        # mv configuration to $PGDATA directory
+        cp /etc/postgresql/$(get_pg_major_version)/main/postgresql.conf $PGDATA/postgresql.conf
+        cp /etc/postgresql/$(get_pg_major_version)/main/pg_hba.conf $PGDATA/pg_hba.conf
+        cp /etc/postgresql/$(get_pg_major_version)/main/pg_ident.conf $PGDATA/pg_ident.conf
+
+      )
+
+}
+
+function is_pg_user_exists()
+{
+    _CMD="psql postgres -tAc \"SELECT 1 FROM pg_roles WHERE rolname='$POSTGRES_USER';\""
+    su postgres -l -c "$_CMD" | grep -q 1
+    test $? -eq 0
+}
+
+function pg_create_user()
+{
+    _CMD="psql postgres -tAc \"CREATE USER $POSTGRES_USER WITH CREATEDB NOCREATEUSER ENCRYPTED PASSWORD '$POSTGRESS_PASSWORD';\""
+    su postgres -l -c "$_CMD"
+    test $? -eq 0
+}
+
+function get_pg_hba()
+{
+    #echo -n $(su - postgres -c "psql -t -P format=unaligned -c 'show hba_file';")
+    echo -n /etc/postgresql/$(get_pg_major_version)/main/pg_hba.conf
+}
+
+function get_pg_config()
+{
+    #echo -n $(su - postgres -c "psql -t -P format=unaligned -c 'show config_file';")
+    echo -n /etc/postgresql/$(get_pg_major_version)/main/postgresql.conf
+}
+
+
+function is_pg_hba_configured()
+{
+    grep -q $POSTGRES_DB $(get_pg_hba)
+    test $? -eq 0
+}
+
+function set_pg_config()
+{
+
+    _CON_FILE=$(get_pg_hba)
+    _CAD="# Put your actual configuration here"
+    sed -i "s/$_CAD/$_CAD\\nlocal   $POSTGRES_DB             $POSTGRES_USER                     password\\n/g" $_CON_FILE
+
+
+    for _ELEMENT in $POSTGRES_ALLOW_HOSTS
+    do
+        echo "host all all $_ELEMENT trust" >> $_CON_FILE
+    done
+
+    echo "host all all 172.17.0.0/16 trust" >> $_CON_FILE
+    echo "host all all 127.0.0.1/8 trust" >> $_CON_FILE
+
+    set_listen_addresses "*"
+
+}
+
+
+function db_server_init()
+{
+    export DJANGO_SETTINGS_MODULE=migasfree.settings.production
+
+#    service postgresql start
+
+#    is_pg_user_exists || pg_create_user
+
+#    is_pg_hba_configured || (
+#        set_pg_config
+#        service postgresql restart || :
+#    )
+
+    set_pg_config
+    service postgresql restart
+    is_pg_user_exists || pg_create_user
+
+
+}
+
+
+touch /etc/migasfree-server/.init-db
+
 if ! [ -f /etc/migasfree-server/settings.py ] ; then
     cat <<EOF>> /etc/migasfree-server/settings.py
 DATABASES = {
@@ -25,104 +155,20 @@ fi
 
 cd /etc/migasfree-server
 POSTGRES_HOST=$(python -c "import settings;print settings.DATABASES['default']['HOST']")
-POSTGRES_PORT=$(python -c "import settings;print settings.DATABASES['default']['PORT']") 
-POSTGRES_DB=$(python -c "import settings;print settings.DATABASES['default']['NAME']") 
-POSTGRES_USER=$(python -c "import settings;print settings.DATABASES['default']['USER']") 
-POSTGRES_PASSWORD=$(python -c "import settings;print settings.DATABASES['default']['PASSWORD']") 
+POSTGRES_PORT=$(python -c "import settings;print settings.DATABASES['default']['PORT']")
+POSTGRES_DB=$(python -c "import settings;print settings.DATABASES['default']['NAME']")
+POSTGRES_USER=$(python -c "import settings;print settings.DATABASES['default']['USER']")
+POSTGRES_PASSWORD=$(python -c "import settings;print settings.DATABASES['default']['PASSWORD']")
 
 
 
-set_listen_addresses() {
-    sedEscapedValue="$(echo "$1" | sed 's/[\/&]/\\&/g')"
-    sed -ri "s/^#?(listen_addresses\s*=\s*)\S+/\1'$sedEscapedValue'/" "$PGDATA/postgresql.conf"
-}
+set_TZ
+cron_init
+pg__data_init
+db_server_init
 
-if [ "$1" = 'postgres' ]; then
-    mkdir -p "$PGDATA"
-    chmod 700 "$PGDATA"
-    chown -R postgres "$PGDATA"
-
-    chmod g+s /run/postgresql
-    chown -R postgres /run/postgresql
-
-    # look specifically for PG_VERSION, as it is expected in the DB dir
-    if [ ! -s "$PGDATA/PG_VERSION" ]; then
-        gosu postgres initdb
-
-        # check password first so we can output the warning before postgres
-        # messes it up
-        if [ "$POSTGRES_PASSWORD" ]; then
-            pass="PASSWORD '$POSTGRES_PASSWORD'"
-            authMethod=md5
-        else
-            # The - option suppresses leading tabs but *not* spaces. :)
-            cat >&2 << EOWARN
-                ****************************************************
-                WARNING: No password has been set for the database.
-                         This will allow anyone with access to the
-                         Postgres port to access your database. In
-                         Docker's default configuration, this is
-                         effectively any other container on the same
-                         system.
-
-                         Use "-e POSTGRES_PASSWORD=password" to set
-                         it in "docker run".
-                ****************************************************
-EOWARN
-
-            pass=
-            authMethod=trust
-        fi
-
-        { echo; echo "host all all 0.0.0.0/0 $authMethod"; } >> "$PGDATA/pg_hba.conf"
-
-        # internal start of server in order to allow set-up using psql-client       
-        # does not listen on TCP/IP and waits until start finishes
-        gosu postgres pg_ctl -D "$PGDATA" \
-            -o "-c listen_addresses=''" \
-            -w start
-
-        : ${POSTGRES_USER:=postgres}
-        : ${POSTGRES_DB:=$POSTGRES_USER}
-        export POSTGRES_USER POSTGRES_DB
-
-        if [ "$POSTGRES_DB" != 'postgres' ]; then
-            psql --username postgres << EOSQL
-                CREATE DATABASE "$POSTGRES_DB" ;
-EOSQL
-            echo
-        fi
-
-        if [ "$POSTGRES_USER" = 'postgres' ]; then
-            op='ALTER'
-        else
-            op='CREATE'
-        fi
-
-        psql --username postgres << EOSQL
-            $op USER "$POSTGRES_USER" WITH SUPERUSER $pass ;
-EOSQL
-        echo
-
-        echo
-        for f in /docker-entrypoint-initdb.d/*; do
-            case "$f" in
-                *.sh)  echo "$0: running $f"; . "$f" ;;
-                *.sql) echo "$0: running $f"; psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" < "$f" && echo ;;
-                *)     echo "$0: ignoring $f" ;;
-            esac
-            echo
-        done
-
-        gosu postgres pg_ctl -D "$PGDATA" -m fast -w stop
-        set_listen_addresses '*'
-
-        echo
-        echo 'PostgreSQL init process complete; ready for start up.'
-        echo
-    fi
-
-    exec gosu postgres "$@"
-fi
-
-exec "$@"
+rm /etc/migasfree-server/.init-db
+while :
+do
+    sleep 5
+done
